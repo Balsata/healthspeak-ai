106e52b94574f14ea1d8cde8baa1b00d4b472b7d
Bryan Saenz <109769483+Balsata@users.noreply.github.com>
2025-06-06 16:03:34 -0600

    [FEATURE] Pagina de traducción terminada

--- ui/components/pages/Translation.tsx
+++ ui/components/pages/Translation.tsx
@@ -1,10 +1,12 @@
 import Header from '../../../ui/components/Header';
 import Footer from '../../../ui/components/Footer';
+import TranslationPage from '../TranslationPage';
 
 const Translation = () => {
     return (
         <>
             <Header />
+            <TranslationPage />
             <Footer />
         </>
     );
